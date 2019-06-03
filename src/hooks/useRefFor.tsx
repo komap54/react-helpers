783260906ea298968372d6f578599356463dbb5d
Анисимов Алексей Николаевич <alexey.anisimov@skbkontur.ru>
2019-06-03 11:26:11 +0700

    added display name in enhanced component in useRefFor

--- src/hooks/useRefFor.tsx
+++ src/hooks/useRefFor.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 
 export default function useRefFor<T1>(Component: React.ComponentType<T1>) {
   const ref = React.useRef<T1>(null);
-  const EnhancedComponent = React.useCallback(
+  const EnhancedComponent: React.ComponentType<T1> = React.useCallback(
     props => (
       <Component ref={ref} inputRef={ref} {...props} />
     ),
     [],
   );
+
+  EnhancedComponent.displayName = `${Component.displayName}WithRef`;
 
   return [ref, EnhancedComponent] as [React.Ref<T1>, React.ComponentType<T1>]
 }
