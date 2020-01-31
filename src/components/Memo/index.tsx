aee8109d62f82426c9682e82a36f2bda06c09b1a
alexey <anissoftkun@gmail.com>
2020-01-31 13:12:01 +0700

    added v2

--- src/components/Memo/index.tsx
+++ src/components/Memo/index.tsx
@@ -1,13 +1,34 @@
 import * as React from 'react';
 
-export const Memo: React.FunctionComponent<{ deps?: React.DependencyList }> = ({
-  deps = [],
+export const Memo: React.FunctionComponent<{ deps: React.DependencyList }> = ({
+  deps,
   children,
 }) => {
   const [cache, saveCache] = React.useState(children);
 
   React.useEffect(() => {
     saveCache(children);
+  }, [...deps]);
+
+  return <>{cache}</>;
+};
+
+
+export const MemoV2: React.FunctionComponent<{
+  deps: React.DependencyList;
+  timebudget?: number;
+}> = ({
+  deps,
+  timebudget = 0,
+  children,
+}) => {
+  const [cache, saveCache] = React.useState(children);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      saveCache(children);
+    }, timebudget);
+    return () => clearTimeout(timer);
   }, [...deps]);
 
   return <>{cache}</>;
