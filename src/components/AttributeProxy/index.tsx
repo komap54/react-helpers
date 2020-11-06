e4f07e58b7a1e5debfc32df994e3a2d7f7813edd
Ilya <balabuev09@gmail.com>
2020-11-06 17:51:42 +0700

    root element props

--- src/components/AttributeProxy/index.tsx
+++ src/components/AttributeProxy/index.tsx
@@ -1,50 +1,56 @@
-import * as React from 'react';
+import * as React from 'react'
 
-import { Direction, getElement } from '../../utils';
+import { Direction, getElement } from '../../utils'
 
 export interface AttributeProxyTypes<T> extends React.HTMLProps<T> {
-  direction: Direction;
-  component?: React.ComponentType<any> | string;
-};
+  direction: Direction
+  component?: React.ComponentType<any> | string
+  rootProps: React.HTMLAttributes<HTMLDivElement>
+}
 
 export const AttributeProxy = React.memo(function Attr<T extends HTMLElement = HTMLDivElement>({
   component: Component = 'div',
   direction,
   children,
+  rootProps,
   ...props
 }: React.PropsWithChildren<AttributeProxyTypes<T>>) {
-  const ref = React.useRef<HTMLElement>(null);
+  const ref = React.useRef<HTMLElement>(null)
 
   React.useEffect(() => {
     if (!ref.current || typeof window === 'undefined') {
-      return () => undefined;
+      return () => undefined
     }
-    const element = getElement<T>(ref.current, direction) as T;
+    const element = getElement<T>(ref.current, direction) as T
 
     if (!element) {
-      return () => undefined;
+      return () => undefined
     }
 
-    const { className, style, ...rest } = props;
+    const { className, style, ...rest } = props
 
     if (className && !element.classList.contains(className)) {
-      element.classList.add(className);
+      element.classList.add(className)
     }
 
     if (style) {
-      Object.assign(element.style, style);
+      Object.assign(element.style, style)
     }
 
     Object.entries(rest).forEach(([attr, value]) => {
-      element.setAttribute(attr, value);
-    });
+      element.setAttribute(attr, value)
+    })
 
     return () => {
       if (className) {
-        element.classList.remove(className);
+        element.classList.remove(className)
       }
-    };
-  }, [props, direction]);
+    }
+  }, [props, direction])
 
-  return <Component ref={ref}>{children}</Component>;
-});
+  return (
+    <Component {...rootProps} ref={ref}>
+      {children}
+    </Component>
+  )
+})
